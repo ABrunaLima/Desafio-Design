aaf9c61eabc7c053cdd93c1c8db9af83016701c9
Bruna <brunaljesus94@gmail.com>
2022-09-27 11:29:08 +0100

    create menu bar

---
+++ src/menubar.js
@@ -0,0 +1,11 @@
+
+function Menu(){
+    return(
+        <div>
+            <button onClick={}>Home</button>
+            <button onClick={}>Candidaturas</button>
+            <button onClick={}>Aprendizes</button>
+            <button onClick={}>Empregabilidade</button>
+        </div>
+    )
+}
